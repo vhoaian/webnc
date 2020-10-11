1652b74cd6025cdbb829be9eca331cdffc350b6c
Vo Hoai An <vohoaian.tn@gmail.com>
2020-10-11 23:03:52 +0700

    Route /week2

---
+++ week2/precache-manifest.8cf5c3f0f2b5827b639dd549aad816eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8c287a47281f6cb95addf57e277d81",
+    "url": "/webnc/week2/index.html"
+  },
+  {
+    "revision": "c35602a6eb87114a9588",
+    "url": "/webnc/week2/static/js/2.827857ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webnc/week2/static/js/2.827857ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83916234855e1b735847",
+    "url": "/webnc/week2/static/js/main.a98b7fa2.chunk.js"
+  },
+  {
+    "revision": "1b583caa891bc6739b82",
+    "url": "/webnc/week2/static/js/runtime-main.c718d1fa.js"
+  }
+]);

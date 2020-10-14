fb6d0c75058cde07acfbdaa11f109892e4e6f9fd
Vo Hoai An <vohoaian.tn@gmail.com>
2020-10-14 22:05:05 +0700

    Week 3: production

---
+++ week3/precache-manifest.5c8ea06938a309ae1172e2b6835e9e40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22ff7065f3ca0b9d1cd789d32e4f6c7",
+    "url": "/webnc/week3/index.html"
+  },
+  {
+    "revision": "386b1c87d16a41ee08a0",
+    "url": "/webnc/week3/static/js/2.d24a3040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webnc/week3/static/js/2.d24a3040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d43687b28e673d46696",
+    "url": "/webnc/week3/static/js/main.95277523.chunk.js"
+  },
+  {
+    "revision": "f0484c2db339fe55e3ce",
+    "url": "/webnc/week3/static/js/runtime-main.4caf1393.js"
+  }
+]);

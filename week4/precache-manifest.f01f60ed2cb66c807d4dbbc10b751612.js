96425dd7176c39b0c8535191b1233824f13aa136
Vo Hoai An <vohoaian.tn@gmail.com>
2020-10-19 23:51:24 +0700

    Week 4 is on air

---
+++ week4/precache-manifest.f01f60ed2cb66c807d4dbbc10b751612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d9020df5a984e72623376bfff1b8de",
+    "url": "/webnc/week4/index.html"
+  },
+  {
+    "revision": "3208ec91ff190ac02bb0",
+    "url": "/webnc/week4/static/js/2.18dc15ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webnc/week4/static/js/2.18dc15ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea295960095a650f6ee",
+    "url": "/webnc/week4/static/js/main.bf70676b.chunk.js"
+  },
+  {
+    "revision": "71fe8bb7fdcb1cd22eb1",
+    "url": "/webnc/week4/static/js/runtime-main.d7ed7d08.js"
+  }
+]);

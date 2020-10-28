54fa359369a23278e3650417c5235da7bfb9dfbb
Vo Hoai An <vohoaian.tn@gmail.com>
2020-10-28 21:35:24 +0700

    Week 5: complete

---
+++ week5/precache-manifest.afe721b2624ee83b0c0174d5ce7a5a3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b47137251422c6f9ba2a4145effd6b9",
+    "url": "/webnc/week5/index.html"
+  },
+  {
+    "revision": "53bf944d1fd241aecf4c",
+    "url": "/webnc/week5/static/js/2.f78fefba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/webnc/week5/static/js/2.f78fefba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973804f2c0555b13d61f",
+    "url": "/webnc/week5/static/js/main.c0986702.chunk.js"
+  },
+  {
+    "revision": "8afdcccded3dcb4bc570",
+    "url": "/webnc/week5/static/js/runtime-main.e5033a9d.js"
+  }
+]);

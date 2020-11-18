821a9a312d766ea9fa9ab9c4dea882ae504c3c11
Vo Hoai An <vohoaian.tn@gmail.com>
2020-11-18 20:36:26 +0700

    Update: chinh sua homepage

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -1,13 +1,20 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { Container } from "@material-ui/core";
 
 export default function Welcome() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <main>
-        <h1>Hello</h1>
-      </main>
+      <Container
+        style={{ display: "flex", justifyContent: "center" }}
+        maxWidth="md"
+      >
+        <h1 style={{ textAlign: "center" }}>
+          Chào mừng bạn đến với hệ thống Sprint Retrospective. Vui lòng chọn
+          chức năng đăng nhập để sử dụng dịch vụ.
+        </h1>
+      </Container>
     </React.Fragment>
   );
 }
